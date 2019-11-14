5f76ee96d96a5f00c6c418b9f45f884695c00518
Samuli Rönkkö <37826096+samulironkko@users.noreply.github.com>
2019-11-14 11:37:52 +0200

    Basic enemy AI

---
+++ GAMEEE/Assets/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    Transform target;
+    NavMeshAgent agent;
+    Animator anim;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(distance > 2)
+        {
+            agent.updatePosition = true;
+            agent.SetDestination(target.position);
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            agent.updatePosition = false;
+            anim.SetBool("Walk", false);
+        }
+    }
+}
