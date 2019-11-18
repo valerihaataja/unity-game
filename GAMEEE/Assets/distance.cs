301b5de0c84d52d21a8f801dfe0e7048a2b2dc86
Valeri Haataja <valeri.haataja@hotmail.com>
2019-11-18 19:11:57 +0200

    Level01 in progress, door can "open"

---
+++ GAMEEE/Assets/distance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distance : MonoBehaviour
+{
+    public GameObject Door;
+    public GameObject Player;
+    public float dis;
+    bool hasOpened = false;
+   
+    void Update()
+    {
+        dis = Vector3.Distance(Door.transform.position, Player.transform.position);
+        if(dis < 4 )
+        {
+           
+            Door.SetActive(false);
+            
+        }else 
+        {
+ 
+            Door.SetActive(true);
+        }
+    }
+}
