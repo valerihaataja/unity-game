2adde9aaf0e6c7408e54ef57c4904d7506d6639a
MattiHeinonen <t7hema02@students.oamk.fi>
2019-12-13 16:29:21 +0200

    added stuff to map

--- GAMEEE/Assets/Scripts/PlayerMovement.cs
+++ GAMEEE/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public GameObject gunController;
+    
 
-    [SerializeField] public float speed = 6f;
+     
+    float speed;
+    [SerializeField] public float runSpeed = 6f;
     [SerializeField] public float crouchSpeed = 3f;
     [SerializeField] public float gravity = -9.81f * 3;
     [SerializeField] public float jumpHeight = 1f;
@@ -22,11 +24,13 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -34,14 +38,13 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         playerMovement();
-
-        
     }
 
     
     
     private void playerMovement()
     {
+
         //MOVEMENT
 
         float x = Input.GetAxis("Horizontal");
@@ -53,32 +56,33 @@ public class PlayerMovement : MonoBehaviour
 
 
         //CROUCHING
-        if(isGrounded && Input.GetKey(KeyCode.LeftControl))
+        /*if(isGrounded && Input.GetKey(KeyCode.LeftControl))
         {
             speed = crouchSpeed;
             controller.transform.localScale = new Vector3(1.0f, 0.6f, 1.0f);
-            
-
+           
         }else
         {
             controller.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            speed = 6f;
+            speed = runSpeed;
         }
 
         if(isGrounded == false && Input.GetKey(KeyCode.LeftControl))
         {
-            speed = 6f;
+            speed = runSpeed;
             controller.transform.localScale = new Vector3(1.0f, 0.6f, 1.0f);
         }
         if(isGrounded == false && Input.GetKey(KeyCode.LeftControl ))
         {
             speed = crouchSpeed;
             controller.transform.localScale = new Vector3(1.0f, 0.6f, 1.0f);
-        }
+        }*/
 
         //WALKING
         if(isGrounded && Input.GetKey(KeyCode.LeftShift)) {
             speed = crouchSpeed;
+        }else{
+            speed = runSpeed;
         }
 
         //JUMPING 
@@ -108,6 +112,7 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+        Debug.Log(velocity.magnitude);
 
     }
 }
---
+++ GAMEEE/Assets/footsteps.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    public CharacterController cc;
+    public AudioSource audioSource;
+    bool isRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetState();
+        PlayAudio();
+    }
+    public void GetState(){
+        if(cc.isGrounded && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftControl))
+            {
+                isRunning = false;
+            }else
+            {
+                isRunning = true;
+            }
+        }else isRunning = false;
+    }
+
+    public void PlayAudio(){
+        if(isRunning){
+            audioSource.loop = true;
+            audioSource.pitch = Random.Range(0.7f, 1.0f);
+            audioSource.volume = Random.Range(0.1f, 0.5f);
+            audioSource.enabled = true;
+        }else {
+            audioSource.loop = false;
+            audioSource.enabled = false;
+        }
+    }
+}
